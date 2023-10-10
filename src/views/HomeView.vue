2db7d890214ae08082638f9f1805b0e3351818c5
Danil <danil.kersontsev@khk.ee>
2023-10-10 13:01:08 +0300

    Initialize project using Create vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>Welcome to the Online Learning Management System</h1>
+    <a href="/registration" class="btn btn-primary" id="register">Register</a>
+    <a href="/login" class="btn btn-secondary" id="login">Login</a>
+  </main>
+</template>
