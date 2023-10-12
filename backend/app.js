27c6ae573601526a917595e7c22f7b82b24074f7
Danil <danil.kersontsev@khk.ee>
2023-10-12 14:52:32 +0300

    Successful backend registration, improved frontend registration page

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const { registerUser } = require('./controllers/registerController'); // Impordi kontroller
 const mysql = require('mysql');
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
+
+// MySQL andmebaasi ühendus
+const dbConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+const { registerUser } = require('./controllers/registerController'); // Impordi kontroller
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Registreerimismarsruut
-app.post('/register', registerUser); // Kasutage registreerimiskontrollerit
+app.post('/api/register', (req, res) => {
+    registerUser(req, res, dbConnection); // Edastage andmebaasiühendus kontrollerile
+});
 
-
-
-// Ülejäänud Express.js seadistused ja marsruudid
-// ...
-
-// Kuulake serveri käivitamiseks määratud porti
 app.listen(port, () => {
     console.log(`Server on käivitatud portil ${port}`);
 });
--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,45 +1,61 @@
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
-const mysql = require('mysql');
-const dotenv = require("dotenv");
-dotenv.config();
-// MySQL andmebaasi ühendus
-const dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+// Ülejäänud impordid jäävad samaks
 
 // Registreerimine
-exports.registerUser = (req, res) => {
+exports.registerUser = (req, res, dbConnection) => {
     const { username, email, password } = req.body;
 
-    // Kui mingeid andmeid pole saadetud või need on puudulikud
-    if (!username || !email || !password) {
-        return res.status(400).json({ error: 'Palun täitke kõik väljad' });
+    // Valideerige andmed kasutades Express Validatorit
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    // Kontrollige, kas kasutaja on juba olemas
-    const checkUserQuery = 'SELECT * FROM users WHERE email = ?';
-    dbConnection.query(checkUserQuery, [email], (checkErr, checkResults) => {
-        if (checkErr) {
-            console.error('Andmete kontrollimine ebaõnnestus: ', checkErr);
-            return res.status(500).json({ error: 'Andmete kontrollimine ebaõnnestus' });
+    // Kontrollige, kas kasutajanimi on juba olemas
+    const checkUsernameQuery = 'SELECT * FROM users WHERE username = ?';
+    dbConnection.query(checkUsernameQuery, [username], (checkUserErr, checkUserResults) => {
+        if (checkUserErr) {
+            console.error('Kasutajanime kontroll ebaõnnestus: ', checkUserErr);
+            return res.status(500).json({ error: 'Kasutajanime kontroll ebaõnnestus' });
         }
 
-        if (checkResults.length > 0) {
-            return res.status(400).json({ error: 'Kasutaja on juba registreeritud' });
+        if (checkUserResults.length > 0) {
+            return res.status(400).json({ error: 'Kasutajanimi on juba kasutusel' });
         }
 
-        // Looge uus kasutaja ja salvestage see kasutajate tabelisse
-        const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-        dbConnection.query(insertUserQuery, [username, email, password], (insertErr) => {
-            if (insertErr) {
-                console.error('Andmete lisamine ebaõnnestus: ', insertErr);
-                return res.status(500).json({ error: 'Andmete lisamine ebaõnnestus' });
+        // Kontrollige, kas kasutaja on juba olemas e-posti järgi
+        const checkUserQuery = 'SELECT * FROM users WHERE email = ?';
+        dbConnection.query(checkUserQuery, [email], (checkErr, checkResults) => {
+            if (checkErr) {
+                console.error('Andmete kontrollimine ebaõnnestus: ', checkErr);
+                return res.status(500).json({ error: 'Andmete kontrollimine ebaõnnestus' });
             }
 
-            return res.status(201).json({ message: 'Registreerimine õnnestus' });
+            if (checkResults.length > 0) {
+                return res.status(400).json({ error: 'Kasutaja on juba registreeritud' });
+            }
+
+            // Krüpti parool enne salvestamist
+            bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
+                if (hashErr) {
+                    console.error('Parooli krüptimine ebaõnnestus: ', hashErr);
+                    return res.status(500).json({ error: 'Parooli krüptimine ebaõnnestus' });
+                }
+
+                // Looge uus kasutaja ja salvestage see kasutajate tabelisse
+                const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+                dbConnection.query(insertUserQuery, [username, email, hashedPassword], (insertErr) => {
+                    if (insertErr) {
+                        console.error('Andmete lisamine ebaõnnestus: ', insertErr);
+                        return res.status(500).json({ error: 'Andmete lisamine ebaõnnestus' });
+                    }
+
+                    return res.status(201).json({ message: 'Registreerimine õnnestus' });
+                });
+            });
         });
     });
 };
+
